e51d78a16689cb55bd9d50c99cbdad07865e6c07
chiharum <m.chiharu0611@gmail.com>
2016-08-16 07:57:34 +0900

    20160816 リストビューのアイテムを作り始めたよ

--- app/src/main/java/com/cat/innovativealarmclock/MainActivity.java
+++ app/src/main/java/com/cat/innovativealarmclock/MainActivity.java
@@ -2,12 +2,17 @@ package com.cat.innovativealarmclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView newsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newsList = (ListView)findViewById(R.id.newsList);
     }
 }
---
+++ app/src/main/java/com/cat/innovativealarmclock/ScheduleItem.java
@@ -0,0 +1,13 @@
+package com.cat.innovativealarmclock;
+
+public class ScheduleItem {
+
+    String scheduleItem;
+    int date;
+
+    public ScheduleItem (String scheduleItem, int date){
+
+        this.scheduleItem = scheduleItem;
+        this.date = date;
+    }
+}
